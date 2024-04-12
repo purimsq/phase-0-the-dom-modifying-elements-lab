9f798b59b77213ca0e56bb4b58da773bf15e542b
Dylen Gitaka <dylen.gitaka@student.moringaschool.com>
2024-04-12 16:30:47 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let newHeader = document.createElement("h1");
+let appendHere = document.getElementById("appear");
+appendHere.append(newHeader);
+newHeader.id("victory"); 
+
+newHeader.textContent = "Dylen is a champion";
